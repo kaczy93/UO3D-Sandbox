5215b6a6f780bc864fc9a931b2960cf3c79c983f
kaczy <kaczyf93@gmail.com>
2025-10-20 09:05:38 +0200

    We have a triangle!

---
+++ SDL3-Sandbox/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace SDL3_Sandbox;
+
+class Program
+{
+
+
+    static void Main(string[] args)
+    {
+        var app = new App();
+
+        app.Init();
+        app.Run();
+    }
+
+}
---
+++ SDL3-Sandbox/Shaders/compile_shaders.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+glslc -fshader-stage=vertex vertex.glsl -o vertex.spv
+glslc -fshader-stage=fragment fragment.glsl -o fragment.spv
